474da012905ce2ec7090b5ce5d94d933de420a74
KevinKipkorir254 <kipkorir.magut254@gmail.com>
2023-11-15 16:59:04 +0300

    This is the starting point for the changes I am going to make for the kdl

---
+++ build/robotic_arm_custom_nodes/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/kevo/ros_ws/src/ai_based_sorting_robot_arm/install/robotic_arm_custom_nodes'
---
+++ build/robotic_arm_moveit_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/kevo/ros_ws/src/ai_based_sorting_robot_arm/install/robotic_arm_description/share/robotic_arm_description/package.sh"
